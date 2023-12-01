f37eae0d0f088b3685bfa7bb39b0527f61b01b9e
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-12-01 06:49:09 +0300

    adding education info data

---
+++ src/data/educationInfo.js
@@ -0,0 +1,26 @@
+export const education = [
+  {
+    course: "Software Engineering",
+    takenFrom: "BahirdarUniversity-BIT",
+    year: "2019-2024",
+  },
+  {},
+  {},
+  {
+    course: "Software Engineering",
+    takenFrom: "BahirdarUniversity-BIT",
+    year: "2019-2024",
+  },
+  {
+    course: "Software Engineering",
+    takenFrom: "BahirdarUniversity-BIT",
+    year: "2019-2024",
+  },
+  {},
+  {},
+  {
+    course: "Software Engineering",
+    takenFrom: "BahirdarUniversity-BIT",
+    year: "2019-2024",
+  },
+];
