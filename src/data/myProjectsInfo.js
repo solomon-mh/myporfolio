c17cd5cab559e305861fc8995b8dbb2625fe4419
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-12-10 09:27:53 +0300

    adding my project info

---
+++ src/data/myProjectsInfo.js
@@ -0,0 +1,26 @@
+import projOne from "../assets/sample/image-generator.png";
+import projTwo from "../assets/sample/ai-chat.png";
+import projThree from "../assets/sample/jibyte-writers.png";
+export const myProjects = [
+  {
+    id: 1,
+    projectTitle: "JiByte Ai Clone",
+    image: projOne,
+    projectType: "Business Website",
+    desc: "Dynamic business sites that reflect your brand and engage customer.",
+  },
+  {
+    id: 2,
+    projectTitle: "E-commerce Website Template",
+    image: projTwo,
+    projectType: "Business Website",
+    desc: "Dynamic business sites that reflect your brand and engage customer.",
+  },
+  {
+    id: 3,
+    projectTitle: "#Vanlife",
+    image: projThree,
+    projectType: "Business Website",
+    desc: "Dynamic business sites that reflect your brand and engage customer.",
+  },
+];
