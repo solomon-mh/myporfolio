72c3c4018baa54c920be8e7e90511c08d4577bbe
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-12-10 14:48:41 +0300

    modfying app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import AboutMe from "./components/AboutMe";
+import Header from "./components/Header";
 import Hero from "./components/Hero";
-import MyImage from "./components/MyImage";
 import MyQualification from "./components/MyQualification";
 import MySkills from "./components/MySkills";
-import MySocialLinks from "./components/MySocialLinks";
 import Projects from "./components/Projects";
 import ScrollDown from "./utils/ScrollDown";
 
 function App() {
   return (
-    <div className='App w-4/5 m-auto my-24 mt-12 '>
-      <header className='py-12 w-4/5 m-auto flex justify-around items-center'>
-        <MySocialLinks />
-        <MyImage />
+    <div className='App sm:w-4/5 m-auto my-24 mt-12 '>
+      <header>
+        <Header />
       </header>
       <main>
         <Hero />
