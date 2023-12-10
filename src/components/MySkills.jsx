1c9a5a917bf06539eeb0628964a51a780d2e9ffb
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-12-10 09:28:43 +0300

    modfying to make it responsive

--- src/components/MySkills.jsx
+++ src/components/MySkills.jsx
@@ -12,11 +12,11 @@ const MySkills = () => {
     <section className='text-center'>
       <h2 className='text-2xl font-semibold font-mono'>Skills</h2>
       <small>My technical skills</small>
-      <div className='skills w-4/5 m-auto grid grid-cols-3 gap-4 px-12 py-6'>
+      <div className='skills w-fit sm:w-4/5 m-auto grid  grid-cols-3 gap-4 px-12 py-6'>
         {mySkills.map((skill) => {
           return (
             <div
-              className='bg-indigo-300 cursor-pointer duration-300 py-1 rounded hover:bg-indigo-400'
+              className='bg-indigo-300 text-sm px-2 sm:text-base cursor-pointer duration-300 py-1 rounded hover:bg-indigo-400'
               key={crypto.randomUUID()}
             >
               {skill}
