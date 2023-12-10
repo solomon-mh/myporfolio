71146416a40ef39112eec007918d96a4497266e0
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-12-10 20:18:31 +0300

    adding scrollPosition data

---
+++ src/data/scrollPosition.js
@@ -0,0 +1,24 @@
+export const homePosition = {
+  min: 0,
+  max: 440,
+};
+export const AboutPosition = {
+  min: 441,
+  max: 770,
+};
+export const SkillsPosition = {
+  min: 775,
+  max: 1040,
+};
+export const ServicePosition = {
+  min: 1042,
+  max: 1580,
+};
+export const ProjectPosition = {
+  min: 1581,
+  max: 2270,
+};
+export const ContactPosition = {
+  min: 2271,
+  max: 3000,
+};
