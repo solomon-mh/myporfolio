63f33d451d8d7a84688cf45076fc092669323b1f
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-12-11 11:24:04 +0300

    some mod on Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,10 @@
-import { faFileText } from "@fortawesome/free-solid-svg-icons";
+import { faMailBulk } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import MyImage from "./MyImage";
 const Hero = () => {
   return (
-    <section>
-      <article className='flex flex-col-reverse text-center sm:text-justify sm:grid sm:grid-cols-2 justify-around items-center mt-44 mb-24'>
+    <section id='home'>
+      <article className='flex flex-col-reverse text-center sm:text-justify sm:grid sm:grid-cols-2 justify-around items-center sm:mt-44 mb-24'>
         <article className='mx-12'>
           <h1 className='text-3xl font-extrabold'>
             Hi, I am{" "}
@@ -22,7 +22,7 @@ const Hero = () => {
           <button className='bg-indigo-600 my-8 p-2 text-sm rounded-lg text-white'>
             Contact Me
             <i className='px-2'>
-              <FontAwesomeIcon icon={faFileText} />
+              <FontAwesomeIcon icon={faMailBulk} />
             </i>
           </button>
         </article>
