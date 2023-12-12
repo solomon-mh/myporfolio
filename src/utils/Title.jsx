28e65884d22464b3c512b251010b20a267d5d6ef
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-12-12 09:35:55 +0300

    adding new util section Title.jsx

---
+++ src/utils/Title.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+const Title = ({ title, subTitle }) => {
+  return (
+    <div className='my-4'>
+      <h1 className='text-4xl font-mono font-semibold py-2'>{title}</h1>
+      <small className='inline-block font-serif text-orange-400 pb-8'>
+        {subTitle}
+      </small>
+    </div>
+  );
+};
+
+export default Title;
