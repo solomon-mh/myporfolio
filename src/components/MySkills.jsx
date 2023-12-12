2e6daa49fff314066defbe5338012c6f2a78e661
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-12-12 09:37:57 +0300

    modfying MySkills.jsx to Support RWD

--- src/components/MySkills.jsx
+++ src/components/MySkills.jsx
@@ -1,4 +1,27 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Title from "../utils/Title";
+import {
+  faBootstrap,
+  faCss3,
+  faHtml5,
+  faJava,
+  faJs,
+  faNode,
+  faPhp,
+  faReact,
+} from "@fortawesome/free-brands-svg-icons";
+
 const MySkills = () => {
+  const skillsIcons = {
+    HTML5: faHtml5,
+    Css: faCss3,
+    Bootstrap: faBootstrap,
+    Javascript: faJs,
+    React: faReact,
+    Node: faNode,
+    java: faJava,
+    Php: faPhp,
+  };
   const mySkills = [
     "HTML",
     "Css",
@@ -7,19 +30,39 @@ const MySkills = () => {
     "ReactJs",
     "BootStrap",
     "MUI",
+    "Node.Js",
+    "MongoDB",
+    "MySQL",
+    "Java",
+    "Php",
   ];
   return (
-    <section className='text-center'>
-      <h2 className='text-2xl font-semibold font-mono'>Skills</h2>
-      <small>My technical skills</small>
-      <div className='skills w-fit sm:w-4/5 m-auto grid  grid-cols-3 gap-4 px-12 py-6'>
+    <section id='skills' className='text-center'>
+      <Title title='Skills' subTitle='My technical skills' />
+      <div className='skills w-fit md:hidden sm:w-4/5 m-auto grid  grid-cols-3 gap-4 px-12 py-6'>
         {mySkills.map((skill) => {
           return (
             <div
-              className='bg-indigo-300 text-sm px-2 sm:text-base cursor-pointer duration-300 py-1 rounded hover:bg-indigo-400'
+              className='bg-indigo-400 text-sm px-2 sm:text-base cursor-pointer duration-500 py-1 rounded hover:bg-indigo-600'
               key={crypto.randomUUID()}
             >
               {skill}
+            </div>
+          );
+        })}
+      </div>
+      {/* For Larger Devices */}
+      <div className=' hidden md:flex md:w-4/5 lg:w-3/5 my-12 mb-24 cursor-pointer flex-wrap m-auto gap-12 justify-center'>
+        {Object.entries(skillsIcons).map(([name, icon]) => {
+          return (
+            <div
+              className='skill-icons duration-500 w-32 h-32 flex flex-col justify-center items-center rounded-full border-2 '
+              key={crypto.randomUUID()}
+            >
+              <div className='text-4xl '>
+                <FontAwesomeIcon icon={icon} />
+              </div>
+              <p>{name}</p>
             </div>
           );
         })}
