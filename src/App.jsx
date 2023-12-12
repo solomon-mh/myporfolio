bb737f1ce5123c88c39b99babc56ca6967b12fe3
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-12-12 09:36:32 +0300

    modfying App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
 import AboutMe from "./components/AboutMe";
+import ContactMe from "./components/ContactMe";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import MyQualification from "./components/MyQualification";
 import MySkills from "./components/MySkills";
 import Projects from "./components/Projects";
+import ProjectsXI from "./components/ProjectsXl";
+import ScrollToTop from "./components/ScrollToTop";
 import ScrollDown from "./utils/ScrollDown";
 
 function App() {
   return (
-    <div className='App sm:w-4/5 m-auto my-24 mt-12 '>
-      <header>
-        <Header />
-      </header>
-      <main>
+    <div className='App'>
+      <Header />
+      <figure>
+        <ScrollToTop />
+      </figure>
+      <main className='md:w-9/10 m-auto md:my-24 mb-24 md:mt-12'>
         <Hero />
         <ScrollDown />
         <AboutMe />
         <MySkills />
         <MyQualification />
-        <Projects />
+        <div className='md:hidden'>
+          <Projects />
+        </div>
+        <div>
+          <ProjectsXI />
+        </div>
       </main>
+      <ContactMe />
+      <Footer />
     </div>
   );
 }
