4d5ed8abe8693bd6ebd064636b006d26b70b0a4e
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-12-12 11:09:59 +0300

    adding new util section TypeWritter.jsx

---
+++ src/utils/TypeWriter.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/prop-types */
+import { useState, useEffect } from "react";
+
+const Typewriter = ({ text, repeatTimes = 4 }) => {
+  const [displayText, setDisplayText] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [repeatCount, setRepeatCount] = useState(0);
+  const [completed, setCompleted] = useState(false);
+
+  useEffect(() => {
+    if (repeatCount < repeatTimes || !completed) {
+      const timer = setTimeout(() => {
+        if (currentIndex < text.length) {
+          setDisplayText((prevText) => prevText + text[currentIndex]);
+          setCurrentIndex((prevIndex) => prevIndex + 1);
+        } else {
+          if (repeatCount < repeatTimes) {
+            setCurrentIndex(0);
+            setDisplayText("");
+            setRepeatCount((prevCount) => prevCount + 1);
+          } else {
+            setCompleted(true);
+          }
+        }
+      }, 100); // Adjust the typing speed by changing the delay here (in milliseconds)
+
+      // Clear timeout when the component unmounts or when the text is fully displayed
+      return () => clearTimeout(timer);
+    }
+  }, [text, currentIndex, repeatCount, repeatTimes, completed]);
+
+  return (
+    <div>
+      <p>{displayText}</p>
+    </div>
+  );
+};
+
+export default Typewriter;
