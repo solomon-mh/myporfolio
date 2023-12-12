8f229e6c3299d2547848b7716bb3868cf51c60d3
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-12-12 13:49:52 +0300

    separating skills data from presentation logic

---
+++ src/data/skills.js
@@ -0,0 +1,34 @@
+import {
+  faBootstrap,
+  faCss3,
+  faHtml5,
+  faJava,
+  faJs,
+  faNode,
+  faPhp,
+  faReact,
+} from "@fortawesome/free-brands-svg-icons";
+export const mySkills = [
+  "HTML",
+  "Css",
+  "JavaScript",
+  "TailwindCss",
+  "ReactJs",
+  "BootStrap",
+  "MUI",
+  "Node.Js",
+  "MongoDB",
+  "MySQL",
+  "Java",
+  "Php",
+];
+export const skillsIcons = {
+  HTML5: faHtml5,
+  Css: faCss3,
+  Bootstrap: faBootstrap,
+  Javascript: faJs,
+  React: faReact,
+  Node: faNode,
+  java: faJava,
+  Php: faPhp,
+};
