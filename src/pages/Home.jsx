7f2fb80b7b04e19292180f32237cf146c053b186
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-12-13 16:43:17 +0300

    adding Home page as a container for all component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,67 @@
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import ScrollDown from "../utils/ScrollDown";
+import AboutMe from "../components/AboutMe";
+import MySkills from "../components/MySkills";
+import MyQualification from "../components/MyQualification";
+import Services from "../components/Services";
+import Projects from "../components/Projects";
+import ProjectsXl from "../components/ProjectsXl";
+import ContactMe from "../components/ContactMe";
+import Footer from "../components/Footer";
+import ScrollToTop from "../components/ScrollToTop";
+import { useEffect, useRef } from "react";
+import ScrollReveal from "scrollreveal";
+
+const Home = () => {
+  const rightRef = useRef(null);
+  const bottomRef = useRef(null);
+
+  useEffect(() => {
+    ScrollReveal().reveal(rightRef.current, {
+      origin: "right",
+      distance: "100px",
+      duration: 2000,
+      delay: 300,
+      rotate: { y: 90 },
+      easing: "ease",
+      reset: true,
+    });
+    ScrollReveal().reveal(bottomRef.current, {
+      origin: "bottom",
+      distance: "100px",
+      duration: 2000,
+      delay: 300,
+      easing: "ease",
+      reset: true,
+    });
+  }, []);
+  return (
+    <div>
+      <Header />
+      <main className='md:w-9/10 m-auto md:my-24 mb-24 md:mt-12'>
+        <Hero />
+        <ScrollDown />
+        <AboutMe />
+        <MySkills />
+        <MyQualification />
+        <Services />
+        <article id='projects' ref={rightRef}>
+          <div className='md:hidden'>
+            <Projects />
+          </div>
+          <div>
+            <ProjectsXl />
+          </div>
+        </article>
+      </main>
+      <div ref={bottomRef}>
+        <ContactMe />
+        <Footer />
+      </div>
+      <ScrollToTop />
+    </div>
+  );
+};
+
+export default Home;
