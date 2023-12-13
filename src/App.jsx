f9037721091be7df0aabc6c9c8cc4c9c9efc5f0c
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-12-13 19:35:51 +0300

    modfying App.jsx to use DarMode toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
+/* eslint-disable no-unused-vars */
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Education from "./components/Education";
 import Work from "./components/Work";
+import { createContext, useState } from "react";
+export const darkModeProvider = createContext();
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+  const darkModeStyle = {
+    backgroundColor: "var(--body-color)",
+    color: "#fff",
+  };
+  const lightModeStyle = {
+    backgroundColor: "#fff",
+    color: "#000",
+  };
+  const isDarkMode = () => {
+    setDarkMode((prev) => !prev);
+  };
   return (
-    <div className='App'>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home />}>
-            <Route index element={<Education />} />
-            <Route path='/work' element={<Work />} />
-          </Route>
-        </Routes>
-      </Router>
+    <div style={darkMode ? darkModeStyle : lightModeStyle}>
+      <darkModeProvider.Provider value={isDarkMode}>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />}>
+              <Route index element={<Education />} />
+              <Route path='/work' element={<Work />} />
+            </Route>
+          </Routes>
+        </Router>
+      </darkModeProvider.Provider>
     </div>
   );
 }
