cebd1712f03b9b5ea6824c28bf2e3d7ce50d8b08
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-12-13 09:22:00 +0300

    modfying Services.jsx to use scrollreveal effect

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,12 +1,31 @@
+import ScrollReveal from "scrollreveal";
 import { faArrowRight, faCode } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Title from "../utils/Title";
+import { useEffect, useRef } from "react";
 
 const Services = () => {
+  const rightRef = useRef(null);
+
+  useEffect(() => {
+    ScrollReveal().reveal(rightRef.current, {
+      origin: "right",
+      distance: "100px",
+      rotate: { y: 90 },
+      duration: 2000,
+      delay: 300,
+      easing: "ease",
+      reset: true,
+    });
+  }, []);
+
   return (
     <section className='my-16 text-center'>
       <Title title='My Services' subTitle='Here is what I can do' />
-      <div className='w-4/5 m-auto grid gap-8 sm:grid-cols-2 py-6 px-12 '>
+      <div
+        ref={rightRef}
+        className='w-4/5 m-auto grid gap-8 sm:grid-cols-2 py-6 px-12 '
+      >
         <div className=' w-54 px-6 py-4 shadow-md shadow-slate-700 cursor-pointer duration-500 hover:scale-105 hover:translate-y-1'>
           <div>
             <div className='mb-2'>
