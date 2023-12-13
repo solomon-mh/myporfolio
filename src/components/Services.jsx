2e940fa0d5ce2bf736b4a18f85fd8ddb45d368e7
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-12-13 09:13:06 +0300

    adding new component Service.jsx

---
+++ src/components/Services.jsx
@@ -0,0 +1,74 @@
+import { faArrowRight, faCode } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Title from "../utils/Title";
+
+const Services = () => {
+  return (
+    <section className='my-16 text-center'>
+      <Title title='My Services' subTitle='Here is what I can do' />
+      <div className='w-4/5 m-auto grid gap-8 sm:grid-cols-2 py-6 px-12 '>
+        <div className=' w-54 px-6 py-4 shadow-md shadow-slate-700 cursor-pointer duration-500 hover:scale-105 hover:translate-y-1'>
+          <div>
+            <div className='mb-2'>
+              <FontAwesomeIcon icon={faCode} />
+            </div>
+            <h2>Frontend Developer</h2>
+            <div
+              onClick={() => alert("Hello")}
+              className='mt-4 text-purple-400'
+            >
+              <button>
+                View More
+                <span className='px-1'>
+                  <FontAwesomeIcon icon={faArrowRight} />
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className='border-slate-700 w-54 px-6 py-4 shadow-md shadow-slate-700 cursor-pointer duration-500 hover:scale-105 hover:translate-y-1'>
+          <div>
+            <div className='mb-2'>
+              <FontAwesomeIcon icon={faCode} />
+            </div>
+            <h2>Backend Developer</h2>
+            <div
+              onClick={() => alert("Hello")}
+              className='mt-4 text-purple-400'
+            >
+              <button>
+                View More
+                <span className='px-1'>
+                  <FontAwesomeIcon icon={faArrowRight} />
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className='border-slate-700 w-54 px-6 py-4 shadow-md shadow-slate-700 cursor-pointer duration-500 hover:scale-105 hover:translate-y-1'>
+          <div>
+            <div className='mb-2'>
+              <FontAwesomeIcon icon={faCode} />
+            </div>
+            <h2>
+              Fullstack Web <br /> Developer
+            </h2>
+            <div
+              onClick={() => alert("Hello")}
+              className='mt-4 text-purple-400'
+            >
+              <button>
+                View More
+                <span className='px-1'>
+                  <FontAwesomeIcon icon={faArrowRight} />
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
