7bb24e18aef881108f783acbc8f1de1ee8c44352
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-12-18 20:14:24 +0300

    separating services presentation logic and data

---
+++ src/data/myService.js
@@ -0,0 +1,32 @@
+export const myServices = [
+  {
+    id: 1,
+    serviceName: "Frontend Developer",
+    services: [
+      "Responsive Web Design - RWD",
+      "Interactive User Interfaces",
+      "SEO - friendly implementations",
+      "Accessibility and UX Enhancement",
+    ],
+  },
+  {
+    id: 2,
+    serviceName: "Backend Developer",
+    services: [
+      "Backend Web Design - RWD",
+      "Interactive User Interfaces",
+      "SEO - friendly implementations",
+      "Accessibility and UX Enhancement",
+    ],
+  },
+  {
+    id: 3,
+    serviceName: "Fullstack  Developer",
+    services: [
+      "Fullstack Web Design - RWD",
+      "Interactive User Interfaces",
+      "SEO - friendly implementations",
+      "Accessibility and UX Enhancement",
+    ],
+  },
+];
