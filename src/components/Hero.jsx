418169023064620175e504c30b87be7ac00933ea
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-12-18 20:22:35 +0300

    some mod on responsiveness on Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
 
     ScrollReveal().reveal(rightRef.current, {
       origin: "right",
-      distance: "50px",
+      distance: "200px",
       duration: 1600,
       delay: 100,
       easing: "ease",
@@ -32,39 +32,40 @@ const Hero = () => {
     " Passionate Fullstack web developer dedicated to provide quality  work in a timely manner.";
 
   return (
-    <section id='home'>
-      <article className='flex flex-col-reverse text-center sm:text-justify md:flex-row justify-around items-center my-8 mx-12 gap-6 sm:my-44'>
-        <article className='mx-16 sm:text-left' ref={leftRef}>
-          <h1 className='text-3xl  md:hidden font-extrabold'>
-            Hi, I am{" "}
-            <span className='text-brightRed'>
-              Solomon <span className='block'>Muhye</span>
-            </span>
-          </h1>
-          <h1 className='hidden text-4xl md:block font-extrabold'>
-            Hi,
-            <br />
-            <span>I am </span>
-            <span className='text-brightRed'>
-              Solomon <span className='block'>Muhye</span>
-            </span>
-          </h1>
-          <small className='font-mono inline-block text-orange-400 py-2'>
-            Fullstack Developer
-          </small>
-          <div>
-            <Typewriter text={textToShow} />
-          </div>
-          <button className='bg-indigo-600 my-8 p-2 text-sm rounded-lg text-white'>
-            Contact Me
-            <i className='px-2'>
-              <FontAwesomeIcon icon={faMailBulk} />
-            </i>
-          </button>
-        </article>
-        <article ref={rightRef}>
-          <MyImage />
-        </article>
+    <section
+      className='flex flex-col-reverse text-center sm:text-justify md:flex-row justify-around items-center my-8 mx-12 gap-12 sm:my-44'
+      id='home'
+    >
+      <article ref={leftRef} className='mx-16 sm:text-left'>
+        <h1 className='text-3xl  md:hidden font-extrabold'>
+          Hi, I am{" "}
+          <span className='text-brightRed'>
+            Solomon <span className='block'>Muhye</span>
+          </span>
+        </h1>
+        <h1 className='hidden text-4xl md:block font-extrabold'>
+          Hi,
+          <br />
+          <span>I am </span>
+          <span className='text-brightRed'>
+            Solomon <span className='block'>Muhye</span>
+          </span>
+        </h1>
+        <small className='font-mono inline-block text-orange-400 py-2'>
+          Fullstack Developer
+        </small>
+        <div>
+          <Typewriter text={textToShow} />
+        </div>
+        <button className='bg-indigo-600 my-8 p-2 text-sm rounded-lg text-white'>
+          Contact Me
+          <i className='px-2'>
+            <FontAwesomeIcon icon={faMailBulk} />
+          </i>
+        </button>
+      </article>
+      <article ref={rightRef}>
+        <MyImage />
       </article>
     </section>
   );
