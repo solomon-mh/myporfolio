3cd49febf2b74b20362b37e06bc1073b691684b1
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-12-18 20:12:30 +0300

    adding new section Work.jsx

---
+++ src/components/Work.jsx
@@ -0,0 +1,16 @@
+const Work = () => {
+  return (
+    <div className='my-12 w-3/5 m-auto'>
+      <h1 className='text-3xl'>My Works</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, eos quae
+        doloremque molestiae ea non dicta nihil cupiditate alias temporibus
+        doloribus animi, distinctio amet ut sunt aut quis esse minima modi natus
+        enim delectus recusandae? Magni incidunt doloremque sit ut reprehenderit
+        at, totam laboriosam!
+      </p>
+    </div>
+  );
+};
+
+export default Work;
