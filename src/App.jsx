c97759608fae1f916753defdfe09dd5cdcf3f550
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-12-18 20:30:50 +0300

    modfying App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,27 +10,39 @@ function App() {
   const darkModeStyle = {
     backgroundColor: "var(--body-color)",
     color: "#fff",
+    width: "100%",
+    overflow: "hidden",
   };
   const lightModeStyle = {
     backgroundColor: "#fff",
     color: "#000",
+    width: "100%",
+    overflow: "hidden",
+    height: "fit-content",
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    padding: 0,
+    margin: 0,
   };
   const isDarkMode = () => {
     setDarkMode((prev) => !prev);
   };
   return (
-    <div style={darkMode ? darkModeStyle : lightModeStyle}>
-      <darkModeProvider.Provider value={isDarkMode}>
-        <Router>
+    <darkModeProvider.Provider value={isDarkMode}>
+      <Router>
+        <div style={darkMode ? darkModeStyle : lightModeStyle}>
           <Routes>
             <Route path='/' element={<Home />}>
               <Route index element={<Education />} />
               <Route path='/work' element={<Work />} />
             </Route>
           </Routes>
-        </Router>
-      </darkModeProvider.Provider>
-    </div>
+        </div>
+      </Router>
+    </darkModeProvider.Provider>
   );
 }
 
