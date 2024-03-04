bbae9ff6c6d732e729e44813f258a7c5b1f05da8
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-03-05 01:45:29 +0300

    modfying footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,19 @@
+import { useRef } from "react";
+import ContactMe from "./ContactMe";
 import MySocialLinks from "./MySocialLinks";
+import scrollreveal from "scrollreveal";
 
 const Footer = () => {
+  const bottomRef = useRef(null);
+  scrollreveal().reveal(bottomRef.current, {
+    origin: "bottom",
+    distance: "400px",
+    duration: 2000,
+    delay: 300,
+    easing: "ease",
+    reset: true,
+  });
+
   const scrollTo = (sectionId) => {
     const section = document.getElementById(sectionId);
     if (section) {
@@ -8,34 +21,39 @@ const Footer = () => {
     }
   };
   return (
-    <div className='px-12 mb-20'>
-      <div className='mb-8'>
-        <h1 className='text-3xl text-green-600'>Solomon Muhye</h1>
-        <small className='font-mono text-orange-400'>Fullstack Developer</small>
+    <footer ref={bottomRef}>
+      <ContactMe />
+      <div className='px-12 mb-20'>
+        <div className='mb-8'>
+          <h1 className='text-3xl text-green-600'>Solomon Muhye</h1>
+          <small className='font-mono text-orange-400'>
+            Fullstack Developer
+          </small>
+        </div>
+        <div className='mb-12 flex flex-col gap-6'>
+          <div
+            className='cursor-pointer duration-300 hover:text-sky-600'
+            onClick={() => scrollTo("services")}
+          >
+            Services
+          </div>
+          <div
+            className='cursor-pointer duration-300 hover:text-sky-600'
+            onClick={() => scrollTo("projects")}
+          >
+            Projects
+          </div>
+          <div
+            className='cursor-pointer duration-300 hover:text-sky-600'
+            onClick={() => scrollTo("contact")}
+          >
+            Contact
+          </div>
+        </div>
+        <MySocialLinks />
+        <p className='text-center py-8'>Solomon Muhye</p>
       </div>
-      <div className='mb-12 flex flex-col gap-6'>
-        <div
-          className='cursor-pointer duration-300 hover:text-sky-600'
-          onClick={() => scrollTo("services")}
-        >
-          Services
-        </div>
-        <div
-          className='cursor-pointer duration-300 hover:text-sky-600'
-          onClick={() => scrollTo("projects")}
-        >
-          Projects
-        </div>
-        <div
-          className='cursor-pointer duration-300 hover:text-sky-600'
-          onClick={() => scrollTo("contact")}
-        >
-          Contact
-        </div>
-      </div>
-      <MySocialLinks />
-      <p className='text-center py-8'>Solomon Muhye</p>
-    </div>
+    </footer>
   );
 };
 
