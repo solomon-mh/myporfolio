cc03fa3117c877acfe525188d905a11a039cde96
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-03-28 14:06:41 +0300

    modfying the modal ui

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,43 +1,32 @@
 /* eslint-disable react/prop-types */
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { myServices } from "../data/myService";
-import { faCheck, faCircle } from "@fortawesome/free-solid-svg-icons";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
 const Modal = ({ id }) => {
-  const indvService = myServices.map((service) => {
-    if (service.id === Number(id)) {
-      return (
-        <div key={service.id} className='py-4 mt-6'>
-          <ul className='my-4 text-left w-fit m-auto'>
-            {service.services.map((service) => (
-              <li
-                key={service.id}
-                className='my-5 text-xs sm:text-base text-left'
-              >
-                <span className='text-purple-600 mr-4 py-1 px-1'>
-                  <span className='fa-layers fa-fw'>
-                    <FontAwesomeIcon
-                      icon={faCircle}
-                      className='text-green-500'
-                      size='lg'
-                    />
-                    <FontAwesomeIcon
-                      icon={faCheck}
-                      className='text-white'
-                      size='sm'
-                      transform='shrink-6'
-                    />
-                  </span>
-                </span>
-                {service}
-              </li>
-            ))}
-          </ul>
-        </div>
-      );
-    }
-  });
-  return <div>{indvService}</div>;
+  const indvService = myServices.find((service) => service.id === Number(id));
+
+  if (!indvService) {
+    return null; // Handle case where id does not match any service
+  }
+
+  return (
+    <div className='py-4 mt-6'>
+      <ul className='my-4 text-left w-fit m-auto'>
+        {indvService.services.map((service, index) => (
+          <li
+            key={index}
+            className='my-3 text-xs sm:text-base flex items-center'
+          >
+            <span className='rounded-full bg-green-500 text-white flex items-center justify-center w-6 h-6 mr-4'>
+              <FontAwesomeIcon icon={faCheck} />
+            </span>
+            <span>{service}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Modal;
