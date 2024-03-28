bfa195120eb9151f58cbd6eac057659706a692f3
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-03-28 14:54:06 +0300

    replacing with sample img

--- src/components/MyImage.jsx
+++ src/components/MyImage.jsx
@@ -1,15 +1,9 @@
-import { faImage } from "@fortawesome/free-solid-svg-icons";
+import myImg from "../assets/myPicture/man.jpg";
 import "../styles/component-stlyes.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const MyImage = () => {
   return (
-    <div>
-      <div className='img__container w-fit m-auto my-12 overflow-hidden p-12 bg-indigo-500'>
-        {/* <img src={myImage} alt='akashi' /> */}
-        <b className='text-9xl relative top-8'>
-          <FontAwesomeIcon icon={faImage} />
-        </b>
-      </div>
+    <div className='img__container w-56 h-56 m-auto my-12 overflow-hidden'>
+      <img className='w-full h-full' src={myImg} alt='my image' />
     </div>
   );
 };
