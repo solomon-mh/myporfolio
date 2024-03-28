3410699bec99328987e07ab454e7c493ce2ee1d1
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-03-28 14:02:17 +0300

    modfying the modal ui

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,24 +1,34 @@
 /* eslint-disable react/prop-types */
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { myServices } from "../data/myService";
-import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { faCheck, faCircle } from "@fortawesome/free-solid-svg-icons";
 
 const Modal = ({ id }) => {
   const indvService = myServices.map((service) => {
     if (service.id === Number(id)) {
-      // console.log(service, id);
       return (
         <div key={service.id} className='py-4 mt-6'>
-          {/* <h1 className='text-xl text-center md:text-3xl mt-12'></h1> */}
           <ul className='my-4 text-left w-fit m-auto'>
             {service.services.map((service) => (
               <li
-                key={crypto.randomUUID()}
-                className='my-5 text-xs sm:text-base'
+                key={service.id}
+                className='my-5 text-xs sm:text-base text-left'
               >
-                <i className='text-purple-600  mr-4 py-1 px-1.5 w-6 h-6 border-2 border-green-500 rounded-full'>
-                  <FontAwesomeIcon icon={faCheck} />
-                </i>
+                <span className='text-purple-600 mr-4 py-1 px-1'>
+                  <span className='fa-layers fa-fw'>
+                    <FontAwesomeIcon
+                      icon={faCircle}
+                      className='text-green-500'
+                      size='lg'
+                    />
+                    <FontAwesomeIcon
+                      icon={faCheck}
+                      className='text-white'
+                      size='sm'
+                      transform='shrink-6'
+                    />
+                  </span>
+                </span>
                 {service}
               </li>
             ))}
