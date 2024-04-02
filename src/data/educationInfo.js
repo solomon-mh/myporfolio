f5ed5c60e4997d3caa7f86b89f77c68c8d34829e
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-04-02 03:45:22 +0300

    modfying my eduaction data

--- src/data/educationInfo.js
+++ src/data/educationInfo.js
@@ -1,23 +1,22 @@
 export const education = [
   {
-    course: "Software Engineering",
-    takenFrom: "BahirdarUniversity-BIT",
-    year: "2019-2024",
-  },
-
-  {
-    course: "Software Engineering",
-    takenFrom: "BahirdarUniversity-BIT",
+    course: "Bachelor of Science in Software Engineering",
+    institution: "Bahirdar University - Institute of Technology",
     year: "2019-2024",
   },
   {
-    course: "Software Engineering",
-    takenFrom: "BahirdarUniversity-BIT",
-    year: "2019-2024",
+    course: "High School Diploma",
+    institution: "Wogdie Preparatory School",
+    year: "2015-2019",
   },
   {
-    course: "Software Engineering",
-    takenFrom: "BahirdarUniversity-BIT",
-    year: "2019-2024",
+    course: "Certification in Programming",
+    institution: "Udemy Certificate For DSA | React | Java",
+    year: "2023",
+  },
+  {
+    course: "Introduction to Computer Science",
+    institution: "Community College or Institution Name",
+    year: "2018",
   },
 ];
