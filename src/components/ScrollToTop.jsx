3760186973bb8df9b73096a42bb8a21f6777a8da
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-04-11 01:42:08 +0300

    modfying the scroll to be at top

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -5,8 +5,6 @@ import { darkModeProvider } from "../App";
 
 const ScrollToTop = () => {
   const { darkMode } = useContext(darkModeProvider);
-  console.clear();
-  console.log(darkMode);
   const [showArrow, setShowArrow] = useState(false);
   const [scrollPosition, setScrollPosition] = useState(0);
   const treshHoldPosition = 150;
@@ -20,16 +18,13 @@ const ScrollToTop = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   });
 
-  const scrollTo = (sectionId) => {
-    const section = document.getElementById(sectionId);
-    if (section) {
-      section.scrollIntoView({ behavior: "smooth" });
-    }
+  const scrollTo = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
   return (
     showArrow && (
       <div
-        onClick={() => scrollTo("nav")}
+        onClick={() => scrollTo()}
         className={`arr__to__top ${
           darkMode ? "" : "bg-slate-200"
         } fixed bottom-20 right-0 w-12 h-12 flex items-center justify-center bg-gray-700 z-50 rounded-full mx-5 cursor-pointer`}
