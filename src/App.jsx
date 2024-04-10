3c569d9d8a20b8053ead92ec6de5a3f2da63bc92
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-04-11 02:26:07 +0300

    moving the styling into different section

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,20 @@ import Home from "./pages/Home";
 import Education from "./components/Education";
 import Work from "./components/Work";
 import { createContext, useState } from "react";
+import { darkModeStyle, lightModeStyle } from "./styles/themeStyles";
+// eslint-disable-next-line react-refresh/only-export-components
 export const darkModeProvider = createContext();
 function App() {
   const [darkMode, setDarkMode] = useState(true);
-  const darkModeStyle = {
-    backgroundColor: "var(--body-color)",
-    color: "#fff",
-    width: "100%",
-    overflow: "hidden",
-  };
-  const lightModeStyle = {
-    backgroundColor: "#fff",
-    color: "#000",
-    width: "100%",
-    overflow: "hidden",
-    height: "fit-content",
-    position: "absolute",
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    padding: 0,
-    margin: 0,
-  };
-  const isDarkMode = () => {
+  const toggleDarkMode = () => {
     setDarkMode((prev) => !prev);
   };
+  const contextValue = {
+    darkMode,
+    toggleDarkMode,
+  };
   return (
-    <darkModeProvider.Provider value={isDarkMode}>
+    <darkModeProvider.Provider value={contextValue}>
       <Router>
         <div style={darkMode ? darkModeStyle : lightModeStyle}>
           <Routes>
