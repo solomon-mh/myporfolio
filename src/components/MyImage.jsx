b4be57f3746f07a7c7d3f8c8a1a15118ec69dac8
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-05-11 23:18:36 +0300

    working on ui

--- src/components/MyImage.jsx
+++ src/components/MyImage.jsx
@@ -2,7 +2,7 @@ import myImg from "../assets/myPicture/man.jpg";
 import "../styles/component-stlyes.css";
 const MyImage = () => {
   return (
-    <div className='img__container w-56 h-56 m-auto my-6 overflow-hidden'>
+    <div className='img__container w-56 h-56 mx-auto overflow-hidden'>
       <img className='w-full h-full' src={myImg} alt='my image' />
     </div>
   );
