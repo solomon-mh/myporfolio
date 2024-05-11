dfd59a15b45f94a5f1e87eb8fc9172c47e65a273
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-05-11 23:20:49 +0300

    adding new components container

---
+++ src/components/componentsModel.js
@@ -0,0 +1,21 @@
+import Header from "./Nav";
+import Hero from "./Hero";
+import AboutMe from "./AboutMe";
+import MySkills from "./MySkills";
+import MyQualification from "./MyQualification";
+import Services from "./Services";
+import Footer from "./Footer";
+import ScrollToTop from "./ScrollToTop";
+import MyProjects from "./MyProjects";
+
+export {
+  Header,
+  Hero,
+  AboutMe,
+  MySkills,
+  MyQualification,
+  Services,
+  Footer,
+  ScrollToTop,
+  MyProjects,
+};
