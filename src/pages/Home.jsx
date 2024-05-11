5a52927bb97f4ca40e3a95a5534a8c8dd9dc587f
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-05-11 23:25:09 +0300

    modfying the structure and ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,16 @@
-import Header from "../components/Header";
-import Hero from "../components/Hero";
-import AboutMe from "../components/AboutMe";
-import MySkills from "../components/MySkills";
-import MyQualification from "../components/MyQualification";
-import Services from "../components/Services";
-// import Projects from "../components/ProjectsSmallDevice";
-// import ProjectsXl from "../components/ProjectsLargeDevice";
-import Footer from "../components/Footer";
-import ScrollToTop from "../components/ScrollToTop";
 import { useEffect, useRef } from "react";
 import ScrollReveal from "scrollreveal";
-import MyProjects from "../components/MyProjects";
-
+import {
+  AboutMe,
+  Footer,
+  Header,
+  Hero,
+  MySkills,
+  MyQualification,
+  MyProjects,
+  ScrollToTop,
+  Services,
+} from "../components/componentsModel";
 const Home = () => {
   const rightRef = useRef(null);
 
@@ -31,23 +30,18 @@ const Home = () => {
       <Header />
       <main className='md:w-9/10 m-auto md:my-24 mb-24 md:mt-12'>
         <Hero />
-        {/* <ScrollDown /> */}
         <AboutMe />
         <MySkills />
         <MyQualification />
         <Services />
-        {/* <article id='projects' ref={rightRef}>
-          <div className='md:hidden'>
-            <Projects />
-          </div>
-          <div>
-            <ProjectsXl />
-          </div>
-        </article> */}
         <MyProjects />
       </main>
-      <Footer />
-      <ScrollToTop />
+      <footer>
+        <Footer />
+      </footer>
+      <aside>
+        <ScrollToTop />
+      </aside>
     </div>
   );
 };
