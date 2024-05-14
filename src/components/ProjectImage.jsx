2b30b74e20fecf1d5f1efdffce877becd332884a
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-05-14 19:17:19 +0300

    adding ScrollReveal effect

--- src/components/ProjectImage.jsx
+++ src/components/ProjectImage.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useRef } from "react";
+import ScrollReveal from "scrollreveal";
 /* eslint-disable react/prop-types */
 function CardImage({ projImg }) {
+  const leftRef = useRef(null);
+
+  useEffect(() => {
+    ScrollReveal().reveal(leftRef.current, {
+      origin: "left",
+      distance: "100px",
+      rotate: { y: 180 },
+      duration: 2000,
+      delay: 300,
+      easing: "ease",
+      reset: true,
+    });
+  }, []);
   return (
-    <div className='rounded-xl overflow-hidden'>
+    <div ref={leftRef} className='rounded-xl overflow-hidden'>
       <img
         src={projImg}
         alt='My Projects image'
