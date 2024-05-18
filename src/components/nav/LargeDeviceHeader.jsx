c234590b68ee95fbd0b28ada156c7d4601bc0cd2
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-05-18 20:55:18 +0300

    working on ui

--- src/components/nav/LargeDeviceHeader.jsx
+++ src/components/nav/LargeDeviceHeader.jsx
@@ -8,7 +8,7 @@ const LargeDeviceHeader = ({ currentSection, scrollToSection }) => {
   const { toggleDarkMode, darkMode } = useContext(darkModeProvider);
 
   return (
-    <header className='hidden sm:flex min-w-full left-0 fixed justify-between items-center z-50 bg-headerColor text-white text-xl py-4 px-8 top-0'>
+    <header className='header hidden sm:flex min-w-full left-0 fixed justify-between items-center z-50 bg-headerColor text-white text-xl py-4 px-8 top-0'>
       <h1>
         Solomon <span className='text-brightRed'>Muhye</span>
       </h1>
@@ -22,7 +22,6 @@ const LargeDeviceHeader = ({ currentSection, scrollToSection }) => {
           Home
         </button>
         <button
-          onClick={() => scrollToSection("about")}
           className={`${
             currentSection === "about" ? "text-brightRed" : ""
           } navlinks`}
@@ -57,7 +56,7 @@ const LargeDeviceHeader = ({ currentSection, scrollToSection }) => {
           {darkMode ? (
             <SunIcon className='w-6 h-6' />
           ) : (
-            <MoonIcon className='w-6 h-6 rotate-180' />
+            <MoonIcon className='w-6 h-6' />
           )}
         </button>
       </nav>
