3f58d1c5ddf0ffaf966e75e0160d111825b771dd
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-05-18 20:05:24 +0300

    moving the projects to new folder

---
+++ src/components/project/ProjectImage.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useRef } from "react";
+import ScrollReveal from "scrollreveal";
+/* eslint-disable react/prop-types */
+function CardImage({ projImg }) {
+  const leftRef = useRef(null);
+
+  useEffect(() => {
+    ScrollReveal().reveal(leftRef.current, {
+      origin: "left",
+      distance: "100px",
+      rotate: { y: 180 },
+      duration: 2000,
+      delay: 300,
+      easing: "ease",
+      reset: true,
+    });
+  }, []);
+  return (
+    <div
+      ref={leftRef}
+      className='w-5/6 sm:w-full mx-auto rounded-xl overflow-hidden'
+    >
+      <img
+        src={projImg}
+        alt='My Projects image'
+        className='w-full h-full object-fit hover:scale-105 duration-200'
+      />
+    </div>
+  );
+}
+
+export default CardImage;
