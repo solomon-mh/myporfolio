83d1ed6ee2b7f4361f8318bbe27bbc93c7cd7d73
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-05-20 20:24:38 +0300

    adding scrollReveal effect

--- src/components/project/ProjectCard.jsx
+++ src/components/project/ProjectCard.jsx
@@ -1,10 +1,26 @@
 /* eslint-disable react/prop-types */
+import { useEffect, useRef } from "react";
+import ScrollReveal from "scrollreveal";
 import ProjectDescription from "./ProjectDescription";
 import ProjectImage from "./ProjectImage";
 
 function Card({ project, index }) {
+  const topRef = useRef(null);
+  useEffect(() => {
+    ScrollReveal().reveal(topRef.current, {
+      origin: "bottom",
+      distance: "300px",
+      duration: 3000,
+      delay: 300,
+      easing: "ease",
+      reset: true,
+    });
+  }, []);
   return (
-    <div className='grid grid-cols-1 w-5/6 rounded-2xl mx-auto md:grid-cols-2 gap-8'>
+    <div
+      ref={topRef}
+      className='grid grid-cols-1 w-4/5 rounded-2xl mx-auto md:grid-cols-2 gap-8'
+    >
       {index % 2 == 0 ? (
         <>
           <ProjectImage projImg={project.image} />
