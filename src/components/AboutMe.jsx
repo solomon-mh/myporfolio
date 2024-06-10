6a79e5887c064d50a4a5682714d28c93d8f45a36
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-06-10 17:11:50 +0300

    changing the layout

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,47 +2,76 @@ import { faCircleArrowDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Title from "../utils/Title";
 import { handleDownload } from "../utils/cvDownloader";
+import coder from "../assets/sample/coder2.jpg";
+import { useContext } from "react";
+import { darkModeProvider } from "../App";
 
 const AboutMe = () => {
+  const { darkMode } = useContext(darkModeProvider);
   return (
-    <section id='about' className='mx-12 px-6 text-center py-5'>
-      <Title title='About Me' subTitle='My introduction' />
-      <article className='md:w-4/5 m-auto leading-7'>
-        <p className='text-left'>
-          Hi, I&apos;m Solomon, a dedicated and passionate software engineer,
-          web and mobile app developer. I thrive on solving problems and
-          creating innovative solutions. With a focus on quality and efficiency,
-          I am committed to delivering high-performance software products. My
-          goal is to make a meaningful impact through technology and to
-          continuously expand my skills to stay at the forefront of the
-          industry.
-        </p>
-        <div className='experience gap-2 sm:gap-8 w-4/5 m-auto flex px-6 mt-6 leading-4 justify-center'>
-          <div className='yrs'>
-            <b className='block font-extrabold text-indigo-600 py-1'>01+</b>
-            <small className='font-extralight italic'>Years Experience</small>
+    <section
+      id='about'
+      className={`${
+        darkMode ? "text-gray-200" : "text-gray-800"
+      } w-4/5 mx-auto py-12 `}
+    >
+      <div className='container mx-auto px-4 max-w-6xl'>
+        <Title title='About Me' subTitle='My introduction' />
+        <section className='lg:flex gap-8 items-center mt-4'>
+          <article className='lg:w-1/2 text-center md:text-left'>
+            <p className='text-lg mb-6 font-sans'>
+              Hi, I&apos;m Solomon, a dedicated and passionate software
+              engineer, web and mobile app developer. I thrive on solving
+              problems and creating innovative solutions. With a focus on
+              quality and efficiency, I am committed to delivering
+              high-performance software products. My goal is to make a
+              meaningful impact through technology and to continuously expand my
+              skills to stay at the forefront of the industry.
+            </p>
+
+            <div className='mb-6 gap-2 sm:gap-8 w-4/5 flex px-6 mt-6 leading-4 justify-left'>
+              <div className='text-center'>
+                <b className='text-sm text-brightRed'>01+</b>
+                <small className='block text-gray-400'>Years Experience</small>
+              </div>
+              <div className='text-center'>
+                <b className='text-sm text-brightRed'>15+</b>
+                <small className='block text-gray-400'>
+                  Completed Projects
+                </small>
+              </div>
+              <div className='text-center'>
+                <b className='text-sm text-brightRed'>01+</b>
+                <small className='block text-gray-400'>Companies Worked</small>
+              </div>
+            </div>
+            <div className='resume-btn text-center md:text-left'>
+              <button
+                onClick={handleDownload}
+                className='relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50'
+              >
+                <span className='absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]' />
+                <span className='inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-blue-500 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl'>
+                  Download CV
+                  <i>
+                    <FontAwesomeIcon icon={faCircleArrowDown} />
+                  </i>{" "}
+                </span>
+              </button>
+            </div>
+          </article>
+          <div className='hidden lg:block md:w-1/2 mt-8 md:mt-0 md:mr-8'>
+            <div className='relative'>
+              <img
+                src={coder}
+                alt='coder'
+                className='rounded-lg shadow-lg transition-transform duration-300 ease-in-out transform hover:scale-105'
+              />
+              <div className='absolute inset-0 bg-black opacity-0 hover:opacity-30 transition-opacity duration-300 rounded-lg'></div>
+            </div>
           </div>
-          <div className='projects'>
-            <b className='block font-extrabold text-indigo-600 py-1'>15+</b>
-            <small className='font-extralight italic'>Completed Project</small>
-          </div>
-          <div className='companies'>
-            <b className='block font-extrabold text-indigo-600 py-1'>01+</b>
-            <small className='font-extralight italic'>Companies Worked</small>
-          </div>
-        </div>
-        <div className='resume-btn'>
-          <button
-            onClick={handleDownload}
-            className='bg-indigo-600 mt-8 p-2 text-sm rounded-lg text-white hover:bg-indigo-700 hover:scale-105 transition duration-300'
-          >
-            Download CV
-            <i className='text-xs px-1'>
-              <FontAwesomeIcon icon={faCircleArrowDown} />
-            </i>
-          </button>
-        </div>
-      </article>
+        </section>
+      </div>
     </section>
   );
 };
