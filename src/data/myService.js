b0f322eed6b3e6b03d70d840ef9e2055dfb44986
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-06-10 17:13:32 +0300

    adding new data to myservice

--- src/data/myService.js
+++ src/data/myService.js
@@ -1,16 +1,76 @@
+import {
+  faReact,
+  faNode,
+  faSass,
+  faPython,
+} from "@fortawesome/free-brands-svg-icons";
+import { faLeaf } from "@fortawesome/free-solid-svg-icons";
+import sample1 from "../assets/sample/coder.jpg";
+import sample2 from "../assets/sample/coder.jpg";
+import sample3 from "../assets/sample/code.png";
+import sample4 from "../assets/sample/coder2.jpg";
+import sample5 from "../assets/sample/coder.jpg";
+
 export const myServices = [
   {
     id: 1,
+    sampleProjects: [
+      {
+        projImg: sample1,
+        desc: "an AI website clone that exactly clone the ui of the jibyteai website with RWD.A project that represents a platform or template designed for creating striking and impressive portfolios or websites, akin to a canvas upon which one can showcase their skills, talents, and achievements in a regal manner.",
+        demoLink: "https://jibyteaiwebsitecloe.netlify.app/",
+        githubLink: "https://github.com/SolomonMuhyeY/jibyteWebsiteClone.git",
+        technologies: [
+          { icon: faSass, name: "Sass" },
+          { icon: faReact, name: "React" },
+          { icon: faNode, name: "Node.js" },
+        ],
+      },
+      {
+        projImg: sample2,
+        desc: "an AI website clone that exactly clone the ui of the jibyteai website with RWD.A project that represents a platform or template designed for creating striking and impressive portfolios or websites, akin to a canvas upon which one can showcase their skills, talents, and achievements in a regal manner.",
+        demoLink: "https://jibyteaiwebsitecloe.netlify.app/",
+        githubLink: "https://github.com/SolomonMuhyeY/jibyteWebsiteClone.git",
+        technologies: [
+          { icon: faSass, name: "Sass" },
+          { icon: faReact, name: "React" },
+          { icon: faNode, name: "Node.js" },
+        ],
+      },
+    ],
     serviceName: "Frontend Developer",
     services: [
       "Responsive Web Design (RWD)",
-      "Modern UI/UX Design",
       "Cross-Browser Compatibility",
       "Performance Optimization",
     ],
   },
   {
     id: 2,
+    sampleProjects: [
+      {
+        projImg: sample3,
+        desc: "an AI website clone that exactly clone the ui of the jibyteai website with RWD.A project that represents a platform or template designed for creating striking and impressive portfolios or websites, akin to a canvas upon which one can showcase their skills, talents, and achievements in a regal manner.",
+        demoLink: "https://jibyteaiwebsitecloe.netlify.app/",
+        githubLink: "https://github.com/SolomonMuhyeY/jibyteWebsiteClone.git",
+        technologies: [
+          { icon: faSass, name: "Sass" },
+          { icon: faReact, name: "React" },
+          { icon: faNode, name: "Node.js" },
+        ],
+      },
+      {
+        projImg: sample4,
+        desc: "an AI website clone that exactly clone the ui of the jibyteai website with RWD.A project that represents a platform or template designed for creating striking and impressive portfolios or websites, akin to a canvas upon which one can showcase their skills, talents, and achievements in a regal manner.",
+        demoLink: "https://jibyteaiwebsitecloe.netlify.app/",
+        githubLink: "https://github.com/SolomonMuhyeY/jibyteWebsiteClone.git",
+        technologies: [
+          { icon: faSass, name: "Sass" },
+          { icon: faReact, name: "React" },
+          { icon: faNode, name: "Node.js" },
+        ],
+      },
+    ],
     serviceName: "Backend Developer",
     services: [
       "RESTful API Development",
@@ -21,32 +81,36 @@ export const myServices = [
   },
   {
     id: 3,
+    sampleProjects: [
+      {
+        projImg: sample5,
+        desc: "an AI website clone that exactly clone the ui of the jibyteai website with RWD.A project that represents a platform or template designed for creating striking and impressive portfolios or websites, akin to a canvas upon which one can showcase their skills, talents, and achievements in a regal manner.",
+        demoLink: "https://jibyteaiwebsitecloe.netlify.app/",
+        githubLink: "https://github.com/SolomonMuhyeY/jibyteWebsiteClone.git",
+        technologies: [
+          { icon: faSass, name: "Sass" },
+          { icon: faReact, name: "React" },
+          { icon: faNode, name: "Node.js" },
+        ],
+      },
+      {
+        projImg: sample1,
+        desc: "an AI website clone that exactly clone the ui of the jibyteai website with RWD.A project that represents a platform or template designed for creating striking and impressive portfolios or websites, akin to a canvas upon which one can showcase their skills, talents, and achievements in a regal manner.",
+        demoLink: "https://jibyteaiwebsitecloe.netlify.app/",
+        githubLink: "https://github.com/SolomonMuhyeY/jibyteWebsiteClone.git",
+        technologies: [
+          { icon: faSass, name: "Sass" },
+          { icon: faReact, name: "React" },
+          { icon: faNode, name: "Node.js" },
+        ],
+      },
+    ],
     serviceName: "Fullstack Developer",
     services: [
       "End-to-End Web Development",
       "Integration of Frontend and Backend",
       "Scalable and Maintainable Architecture",
       "Data Modeling and ORM",
-    ],
-  },
-  {
-    id: 4,
-    serviceName: "Mobile App Developer",
-    services: [
-      "Component-Based Architecture",
-      "State Management Solutions",
-      "Platform-Specific Optimization",
-      "Offline Support and Syncing",
-    ],
-  },
-  {
-    id: 5,
-    serviceName: "UI/Ux Designer",
-    services: [
-      "User Interface (UI) Design",
-      "User Experience (UX) Design",
-      "Wireframing and Prototyping",
-      "Usability Testing and Research",
     ],
   },
 ];
