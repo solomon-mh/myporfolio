f92ff38426810e6c580bc4b24cb21490ae86b1d5
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-07-06 12:23:24 +0300

    modifying the structure of the home page for responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,7 @@ import MyImage from "./MyImage";
 import Typewriter from "../utils/TypeWriter";
 import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 import { TeckStack } from "../components";
+import { styles } from "../styles";
 
 const Hero = () => {
   const topRef = useRef(null);
@@ -20,56 +21,48 @@ const Hero = () => {
     });
   }, []);
   const textToShow =
-    "Passionate Fullstack developer based in Ethiopia, dedicated to providing quality work in a timely manner.";
+    "Passionate developer based in Ethiopia, dedicated to providing quality work in a timely manner.";
 
   return (
-    <div className='pt-20'>
+    <div className="pt-20 h-screen md:pt-32">
       <article
         ref={topRef}
-        className='flex gap-4 sm:gap-16 flex-col-reverse md:flex-row justify-between items-center w-4/5 mr-12 mx-auto'
-        id='home'
+        className="flex gap-2 md:gap-16 flex-col-reverse md:flex-row justify-between items-center w-9/10  mx-auto"
+        id="home"
       >
-        <section className='md:w-1/2 text-center md:text-left'>
-          <h1 className='text-3xl -mb-10 sm:mb-0 md:hidden font-extrabold'>
-            Hi, I am
-            <span className='text-brightRed mx-1'>
-              Solomon <span className='block'>Muhye</span>
-            </span>
+        <section className="md:w-1/2 text-center md:text-left">
+          <h1 className={`${styles.heroHeadText} hero-header text-white`}>
+            Hi, I&apos;m{" "}
+            <span className="hero-header text-[#915EFF]">Solomon</span>
           </h1>
-          <h1 className='hidden text-4xl md:block font-extrabold'>
-            Hi,
-            <br />
-            <span>I am </span>
-            <span className='text-brightRed'>
-              Solomon <span className='block'>Muhye</span>
-            </span>
-          </h1>
-          <p className='font-mono font-extrabold text-lg inline-block text-lime-500 my-4'>
+          <p
+            className={`${styles.heroSubText} hero-header text-xl font-extrabold inline-block text-lime-500 mb-4`}
+          >
             FullStack Developer
           </p>
-          <div className='mb-8 hidden sm:block'>
+          <div className="mb-2 md:mb-8 hidden sm:block">
             <Typewriter text={textToShow} />
           </div>
-          <div className='flex justify-center md:justify-start gap-4'>
+          <div className="flex justify-center md:justify-start gap-4">
             <a
-              href='https://www.linkedin.com/in/solomonmuhye'
-              target='_blank'
-              rel='noopener noreferrer'
+              href="https://www.linkedin.com/in/solomonmuhye"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              <button className='inline-flex h-12 animate-shimmer items-center justify-center rounded-md  bg-[linear-gradient(110deg,#3b6bdc,45%,#7e99d8,65%,#3b6bdc)] bg-[length:200%_100%] px-6 font-medium text-slate-100 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50'>
-                <i className='pr-2'>
+              <button className={`${styles.shimmeringBtnLdn}`}>
+                <i className="pr-2">
                   <FontAwesomeIcon icon={faLinkedinIn} />
                 </i>
                 LinkedIn
               </button>
             </a>
             <a
-              href='https://github.com/SolomonMuhyeY'
-              target='_blank'
-              rel='noopener noreferrer'
+              href="https://github.com/solomon-mh"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              <button className='inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#a0b7d8,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-100 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50'>
-                <i className='pr-2'>
+              <button className={`${styles.shimmeringBtnGit}`}>
+                <i className="pr-2">
                   <FontAwesomeIcon icon={faGithub} />
                 </i>
                 GitHub
@@ -77,12 +70,12 @@ const Hero = () => {
             </a>
           </div>
         </section>
-        <section className='md:w-1/2 md:mb-0'>
+        <section className="md:w-1/2 md:mb-0">
           <MyImage />
         </section>
       </article>
-      <div className='md:w-4/5 flex flex-col my-10 mr-12 mx-auto items-center sm:gap-6 sm:flex-row sm:my-1'>
-        <p className='text-lg font-semibold'>My Tech Stack</p>
+      <div className="mx-12 md:w-4/5 flex flex-col md:my-10 mr-12 md:mx-auto text-[10px] md:text-base items-center sm:gap-6 sm:flex-row">
+        <p className="text-xs md:text-lg font-semibold">My Tech Stack</p>
         <TeckStack />
       </div>
     </div>
