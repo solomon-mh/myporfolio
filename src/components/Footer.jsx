d72a5a7ff8208a01d3266c3711de47ea28c9d92e
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-07-07 12:24:06 +0300

    fixing some broken links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,36 +11,36 @@ const Footer = () => {
   return (
     <>
       <ContactMe />
-      <hr className='my-8' />
-      <div className='w-3/4  mx-auto my-16 mb-4'>
-        <div className='mb-8'>
-          <h1 className='text-3xl text-green-600'>Solomon Muhye</h1>
-          <small className='font-mono text-orange-400'>
+      <hr className="my-8" />
+      <div className="w-3/4  mx-auto my-16 mb-4">
+        <div className="mb-8">
+          <h1 className="text-3xl text-green-600">Solomon Muhye</h1>
+          <small className="font-mono text-orange-400">
             Fullstack Developer
           </small>
         </div>
-        <div className='mb-12 flex flex-col gap-6'>
+        <div className="mb-12 flex flex-col gap-6">
           <div
-            className='cursor-pointer duration-300 hover:text-sky-600'
-            onClick={() => scrollTo("services")}
+            className="cursor-pointer duration-300 hover:text-sky-600"
+            onClick={() => scrollTo("about")}
           >
-            Services
+            About
           </div>
           <div
-            className='cursor-pointer duration-300 hover:text-sky-600'
+            className="cursor-pointer duration-300 hover:text-sky-600"
             onClick={() => scrollTo("projects")}
           >
             Projects
           </div>
           <div
-            className='cursor-pointer duration-300 hover:text-sky-600'
+            className="cursor-pointer duration-300 hover:text-sky-600"
             onClick={() => scrollTo("contact")}
           >
             Contact
           </div>
         </div>
         <MySocialLinks />
-        <p className='text-center py-8'>Solomon Muhye &copy; 2024</p>
+        <p className="text-center py-8">Solomon Muhye &copy; 2024</p>
       </div>
     </>
   );
