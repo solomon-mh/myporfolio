54c6b46045880f313940046c293eb61b450eee90
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-07-10 12:27:07 +0300

    mofying the styling

--- src/components/project/MyProjects.jsx
+++ src/components/project/MyProjects.jsx
@@ -3,11 +3,11 @@ import Title from "../../utils/Title";
 import ProjectCard from "./ProjectCard";
 function MyProjects() {
   return (
-    <div id='projects' className='py-2'>
+    <div id="projects" className="py-2">
       <div>
-        <Title title='Projects' subTitle='most recent projects' />
+        <Title title="Projects" subTitle="most recent projects" />
       </div>
-      <section className='grid grid-cols-1 gap-24'>
+      <section className="grid grid-cols-1 gap-24">
         {myProjects.map((proj, index) => {
           return <ProjectCard key={proj.id} index={index} project={proj} />;
         })}
