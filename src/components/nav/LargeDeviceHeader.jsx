d0d53fa41447e99724717f7677a4fe20c18bc482
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-07-12 12:28:29 +0300

    fixing nav links

--- src/components/nav/LargeDeviceHeader.jsx
+++ src/components/nav/LargeDeviceHeader.jsx
@@ -4,61 +4,68 @@ import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
 import { useContext } from "react";
 import { darkModeProvider } from "../../App";
 import { NavLink } from "react-router-dom";
+import {
+  faChalkboardUser,
+  faContactBook,
+  faHome,
+  faLaptopCode, // Import the new icon
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const LargeDeviceHeader = ({ currentSection, scrollToSection }) => {
   const { toggleDarkMode, darkMode } = useContext(darkModeProvider);
 
   return (
-    <header className='header hidden sm:flex min-w-full left-0 fixed justify-between items-center z-50 bg-headerColor text-white text-xl py-4 px-8 top-0'>
+    <header className="header hidden md:flex min-w-full left-0 fixed justify-between items-center z-50 bg-headerColor text-white text-xl py-4 px-8 top-0">
       <h1>
-        Solomon <span className='text-brightRed'>Muhye</span>
+        Solomon <span className="text-purple-600">Muhye</span>
       </h1>
-      <nav className='flex'>
+      <nav className="flex space-x-6">
         <NavLink
-          onClick={() => scrollToSection("nav")}
-          className={`${
+          onClick={() => scrollToSection("home")}
+          className={`navlinks ${
             currentSection === "home" ? "text-brightRed" : ""
-          } navlinks`}
+          }`}
         >
+          <FontAwesomeIcon icon={faHome} />
+          <br />
           Home
         </NavLink>
         <NavLink
           onClick={() => scrollToSection("about")}
-          className={`${
+          className={`navlinks ${
             currentSection === "about" ? "text-brightRed" : ""
-          } navlinks`}
+          }`}
         >
+          <FontAwesomeIcon icon={faChalkboardUser} />
+          <br />
           About
         </NavLink>
         <NavLink
           onClick={() => scrollToSection("services")}
-          className={`${
+          className={`navlinks ${
             currentSection === "services" ? "text-brightRed" : ""
-          } navlinks`}
+          }`}
         >
-          Services
-        </NavLink>
-        <NavLink
-          onClick={() => scrollToSection("projects")}
-          className={`${
-            currentSection === "projects" ? "text-brightRed" : ""
-          } navlinks`}
-        >
-          Projects
+          <FontAwesomeIcon icon={faLaptopCode} />
+          <br />
+          Services + Projects
         </NavLink>
         <NavLink
           onClick={() => scrollToSection("contact")}
-          className={`${
+          className={`navlinks ${
             currentSection === "contact" ? "text-brightRed" : ""
-          } navlinks`}
+          }`}
         >
+          <FontAwesomeIcon icon={faContactBook} />
+          <br />
           Contact Me
         </NavLink>
-        <button onClick={() => toggleDarkMode()}>
+        <button onClick={() => toggleDarkMode()} className="ml-4">
           {darkMode ? (
-            <SunIcon className='w-6 h-6' />
+            <SunIcon className="w-6 h-6" />
           ) : (
-            <MoonIcon className='w-6 h-6' />
+            <MoonIcon className="w-6 h-6" />
           )}
         </button>
       </nav>
