97082b5016cd7acff2425e1a3a3de3ad70692b49
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-07-19 12:36:23 +0300

    modfying the styling

--- src/components/TeckStack.jsx
+++ src/components/TeckStack.jsx
@@ -39,26 +39,27 @@ const SkillList = () => {
     { icon: faNode, name: "Node.js", type: "fontawesome" },
     { icon: null, name: "DJ", type: "text" },
     { icon: faGitAlt, name: "Git", type: "fontawesome" },
+    // { icon: null, name: "N", type: "text" },
   ];
 
   return (
-    <div className='skill-container my-8 overflow-x-hidden whitespace-nowrap'>
+    <div className="skill-container my-8 overflow-x-hidden whitespace-nowrap">
       <div
         ref={skillListRef}
-        className='skill-list inline-block animate-scroll-x-reverse'
+        className="skill-list inline-block animate-scroll-x-reverse"
       >
         {technologies.map((tech, index) => (
           <div
             key={index}
-            className='inline-block mx-6 rounded group cursor-pointer transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110'
+            className="inline-block mx-6 rounded group cursor-pointer transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110"
           >
             {tech.type === "fontawesome" ? (
               <FontAwesomeIcon
                 icon={tech.icon}
-                className='text-xl text-gray-400 group-hover:text-brightRed transition duration-300 ease-in-out'
+                className="text-xl text-gray-400 group-hover:text-brightRed transition duration-300 ease-in-out"
               />
             ) : (
-              <p className='text-xl text-gray-400 font-semibold group-hover:text-brightRed transition duration-300 ease-in-out'>
+              <p className="text-xl text-gray-400 font-semibold group-hover:text-brightRed transition duration-300 ease-in-out">
                 dj
               </p>
             )}
