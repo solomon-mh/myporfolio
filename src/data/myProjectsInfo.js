256db326cb146b8f492e8d2af9df730157f44a05
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-07-20 12:37:19 +0300

    modfying project info

--- src/data/myProjectsInfo.js
+++ src/data/myProjectsInfo.js
@@ -11,12 +11,12 @@ import { faLeaf } from "@fortawesome/free-solid-svg-icons";
 export const myProjects = [
   {
     id: 1,
-    projectTitle: "JiByte Ai Clone",
+    projectTitle: "JiByte Ai Ui Clone",
     image: jibyteSample,
     projectType: "Business Website",
     desc: "an AI website clone that exactly clone the ui of the jibyteai website with RWD.A project that represents a platform or template designed for creating striking and impressive portfolios or websites, akin to a canvas upon which one can showcase their skills, talents, and achievements in a regal manner.",
     demoLink: "https://jibyteaiwebsitecloe.netlify.app/",
-    githubLink: "https://github.com/SolomonMuhyeY/jibyteWebsiteClone.git",
+    githubLink: "https://github.com/solomon-mh/ai-website-clone",
     technologies: [
       { icon: faSass, name: "Sass" },
       { icon: faReact, name: "React" },
@@ -30,7 +30,7 @@ export const myProjects = [
     projectType: "RegalCanvas",
     desc: "A project that represents a platform or template designed for creating striking and impressive portfolios or websites, akin to a canvas upon which one can showcase their skills, talents, and achievements in a regal manner.",
     demoLink: "https://lustrous-biscuit-fd717c.netlify.app",
-    githubLink: "https://github.com/SolomonMuhyeY/RegalCanvas.git",
+    githubLink: "https://github.com/solomon-mh/regalcanvas.git",
     technologies: [
       { icon: faReact, name: "React" },
       { icon: faNode, name: "Node.js" },
@@ -43,7 +43,7 @@ export const myProjects = [
     projectType: "Business Website",
     desc: "Dynamic business sites that reflect your brand and engage customer. It is a website for car Rent for vacation and other stuff. The site implemts Routing and other things.",
     demoLink: "https://example.com",
-    githubLink: "https://github.com/SolomonMuhyeY/vanlife.git",
+    githubLink: "https://github.com/solomon-mh/vanlife.git",
     technologies: [
       { icon: faReact, name: "React" },
       { icon: faLeaf, name: "Tailwind" },
