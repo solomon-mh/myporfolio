6e30092b517fdd587915da8335678fb4e753239d
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-07-21 12:37:43 +0300

    modfying service info

--- src/data/myService.js
+++ src/data/myService.js
@@ -22,7 +22,7 @@ export const myServices = [
         projImg: [jibyte1, jibyte2, jibyte3, jibyte4],
         desc: "an AI website clone that exactly clone the ui of the jibyteai website with RWD.A project that represents a platform or template designed for creating striking and impressive portfolios or websites, akin to a canvas upon which one can showcase their skills, talents, and achievements in a regal manner.",
         demoLink: "https://jibyteaiwebsitecloe.netlify.app/",
-        githubLink: "https://github.com/SolomonMuhyeY/jibyteWebsiteClone.git",
+        githubLink: "https://github.com/solomon-mh/jibyteWebsiteClone.git",
         technologies: [
           { icon: faSass, name: "Sass" },
           { icon: faReact, name: "React" },
@@ -33,7 +33,7 @@ export const myServices = [
         projImg: [sample1, jibyte2, sample2, jibyte4],
         desc: "an AI website clone that exactly clone the ui of the jibyteai website with RWD.A project that represents a platform or template designed for creating striking and impressive portfolios or websites, akin to a canvas upon which one can showcase their skills, talents, and achievements in a regal manner.",
         demoLink: "https://jibyteaiwebsitecloe.netlify.app/",
-        githubLink: "https://github.com/SolomonMuhyeY/jibyteWebsiteClone.git",
+        githubLink: "https://github.com/solomon-mh/jibyteWebsiteClone.git",
         technologies: [
           { icon: faSass, name: "Sass" },
           { icon: faReact, name: "React" },
@@ -46,6 +46,11 @@ export const myServices = [
       "Responsive Web Design (RWD)",
       "Cross-Browser Compatibility",
       "Performance Optimization",
+      "Client-Side Frameworks (React, Angular, Vue)",
+      "UX/UI Design",
+      "Progressive Web Apps (PWAs)",
+      "State Management (Redux, MobX)",
+      "CSS Preprocessing (Sass, Less)",
     ],
   },
   {
@@ -55,7 +60,7 @@ export const myServices = [
         projImg: [sample4, sample5, jibyte3, sample1],
         desc: "A project that represents a platform or template designed for creating striking and impressive portfolios or websites, akin to a canvas upon which one can showcase their skills, talents, and achievements in a regal manner.",
         demoLink: "https://lustrous-biscuit-fd717c.netlify.app",
-        githubLink: "https://github.com/SolomonMuhyeY/RegalCanvas.git",
+        githubLink: "https://github.com/solomon-mh/RegalCanvas.git",
         technologies: [
           { icon: faReact, name: "React" },
           { icon: faNode, name: "Node.js" },
@@ -65,7 +70,7 @@ export const myServices = [
         projImg: [sample1, jibyte2, sample1, jibyte4],
         desc: "A project that represents a platform or template designed for creating striking and impressive portfolios or websites, akin to a canvas upon which one can showcase their skills, talents, and achievements in a regal manner.",
         demoLink: "https://lustrous-biscuit-fd717c.netlify.app",
-        githubLink: "https://github.com/SolomonMuhyeY/RegalCanvas.git",
+        githubLink: "https://github.com/solomon-mh/RegalCanvas.git",
         technologies: [
           { icon: faReact, name: "React" },
           { icon: faNode, name: "Node.js" },
@@ -75,9 +80,13 @@ export const myServices = [
     serviceName: "Backend Developer",
     services: [
       "RESTful API Development",
-      "Database Management",
+      "Database Management (SQL, NoSQL)",
       "Server-Side Optimization",
       "Security and Authentication",
+      "Microservices Architecture",
+      "Cloud Integration (AWS, Azure)",
+      "DevOps Practices (CI/CD, Docker, Kubernetes)",
+      "Automated Testing (Unit, Integration, End-to-End)",
     ],
   },
   {
@@ -87,7 +96,7 @@ export const myServices = [
         projImg: [jibyte1, sample1, jibyte3, sample4],
         desc: "Dynamic business sites that reflect your brand and engage customer. It is a website for car Rent for vacation and other stuff. The site implements Routing and other things.",
         demoLink: "https://example.com",
-        githubLink: "https://github.com/SolomonMuhyeY/vanlife.git",
+        githubLink: "https://github.com/solomon-mh/vanlife.git",
         technologies: [
           { icon: faReact, name: "React" },
           { icon: faLeaf, name: "Tailwind" },
@@ -98,7 +107,7 @@ export const myServices = [
         projImg: [sample1, jibyte2, sample1, jibyte4],
         desc: "an AI website clone that exactly clone the ui of the jibyteai website with RWD.A project that represents a platform or template designed for creating striking and impressive portfolios or websites, akin to a canvas upon which one can showcase their skills, talents, and achievements in a regal manner.",
         demoLink: "https://jibyteaiwebsitecloe.netlify.app/",
-        githubLink: "https://github.com/SolomonMuhyeY/jibyteWebsiteClone.git",
+        githubLink: "https://github.com/solomon-mh/jibyteWebsiteClone.git",
         technologies: [
           { icon: faSass, name: "Sass" },
           { icon: faReact, name: "React" },
@@ -111,7 +120,11 @@ export const myServices = [
       "End-to-End Web Development",
       "Integration of Frontend and Backend",
       "Scalable and Maintainable Architecture",
-      "Data Modeling and ORM",
+      "Data Modeling and ORM (Mongoose, Sequelize)",
+      "Version Control (Git, GitHub)",
+      "Real-Time Applications (WebSockets, Socket.io)",
+      "API Design and Documentation (Swagger, OpenAPI)",
+      "Continuous Deployment and Monitoring",
     ],
   },
 ];
